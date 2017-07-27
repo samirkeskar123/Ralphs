497ea252d02c6a96492caebe09070817b74e8b32
Samir Personal <Samir Personal@10.0.0.14>
2017-07-26 20:03:45 -0700

    Created new test for Store zip validations

--- src/test/java/PageObject/LandingPage.java
+++ src/test/java/PageObject/LandingPage.java
@@ -11,6 +11,10 @@ public class LandingPage {
 	By Signin = By.cssSelector("#root > div > div.Page-sidebar > div > div > div.SignInPanel > div:nth-child(2) > button");
     By Search = By.cssSelector("#root > div > div.Page-top > div.Header.Page-header > div > div.DesktopHeader-primary > div.DesktopHeader-primary-left > div.DesktopHeader-search > div > div > form > span > input");
     By SearchButton = By.cssSelector(".Input-button");
+    By StoreFinderLink = By.cssSelector(".NoPreferredStore-findStore");
+    By StoreFinderZip = By.cssSelector("#DropdownSearch-searchInput");
+    By StoreFinderZipbutton = By.cssSelector(".DropdownSearch-searchButton");
+    By CityZip = By.xpath(".//*[@id='root']/div/div[3]/div[2]/div/div[1]/div[2]/div[1]/div[2]/div/div/div[3]/div[2]/div/div[2]/div[1]/div[1]/a");
     
     //Creating Constructor
     public LandingPage(WebDriver driver) {
@@ -18,7 +22,7 @@ public class LandingPage {
 	
 	}
     
-
+    //Methods
     public WebElement getLogin(){
 		return driver.findElement(Signin);
     	
@@ -33,4 +37,19 @@ public class LandingPage {
     	return driver.findElement(SearchButton);
     }
     
+    public WebElement Storelinkfinder() {
+    	return driver.findElement(StoreFinderLink);
+    }
+    
+    public WebElement Storelinkzip(){
+    	return driver.findElement(StoreFinderZip);
+    }
+    
+    public WebElement StoreFinderZipbutton(){
+    	return driver.findElement(StoreFinderZipbutton);
+    }
+    
+    public WebElement StoreCityName() {
+    	return driver.findElement(CityZip);
+    }
 }
--- src/test/java/Ralphs/HomePage.java
+++ src/test/java/Ralphs/HomePage.java
@@ -1,5 +1,6 @@
 package Ralphs;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -8,11 +9,16 @@ import org.testng.annotations.Test;
 import PageObject.LandingPage;
 import PageObject.LoginPage;
 
+
+import static org.testng.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.apache.commons.logging.impl.Log4JLogger;
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import resources.base;
 
@@ -41,6 +47,7 @@ public class HomePage extends base {
 		lp.getloginbutton().click();
 	}
 	
+	//Test to search for data on Landing page
 	@Test (dataProvider = "SearchData")
 	public void search(String Search) throws InterruptedException {
 
@@ -52,6 +59,52 @@ public class HomePage extends base {
 		l2.getSearch().sendKeys(Search);
 		l2.SearchButton().click();
 	}
+	
+	//Test to search store by using Zipcode using City
+	@Test (dataProvider = "Storezipsearch")
+	public void storefinder(String Storezipsearch) throws InterruptedException{
+		
+		driver.get(prop.getProperty("url"));
+		LandingPage l3 = new LandingPage(driver);
+		l3.Storelinkfinder().click();
+		Thread.sleep(1000);
+		System.out.println("Storelink is clicked");
+		l3.Storelinkzip().sendKeys(Storezipsearch);
+		l3.StoreFinderZipbutton().click();
+		
+	}
+		
+	
+	//Test to search store by using zipcode using Number zip
+	
+		@Test (dataProvider = "Storezipsearch1")
+		public void storefinder1(String Storezipsearchnew) throws InterruptedException{
+			
+			driver.get(prop.getProperty("url"));
+			LandingPage l4 = new LandingPage(driver);
+			l4.Storelinkfinder().click();
+			Thread.sleep(1000);
+			System.out.println("Storelink by zipcode is clicked");
+			l4.Storelinkzip().sendKeys( Storezipsearchnew);
+		    l4.StoreFinderZipbutton().click();
+		    String City= l4.StoreCityName().getText();
+		    if(City == "90032"){
+		    	System.out.println("City is El Sereno");
+		    } 
+		    
+		    else {
+		    	System.out.println("Wrong Results for the zipcode of 90032");
+		    }
+			
+		    if(City == "95035"){
+		    	System.out.println("City is Milpitas");
+		    } 
+		    
+		    else {
+		    	System.out.println("Wrong Results for the zipcode of 95035");
+		    }
+		}
+			
 	
 	
 	@AfterTest
@@ -87,6 +140,32 @@ public class HomePage extends base {
 			
 			new Object[] {"coke"},
 			new Object[] {"Pepsi"}
+			
+		};
+		
+	}
+	
+	@DataProvider
+	public Object[][] Storezipsearch(){
+			
+		return new Object[][]{
+				
+			new Object[] {"San Jose"},
+			new Object[] {"Milpitas"},
+		
+			};
+	}
+	
+	
+	@DataProvider
+	public Object[][] Storezipsearch1(){
+			
+     return new Object[][]{
+			
+			new Object[] {"90032"},
+			new Object[] {"95035"}
+			
 		};
 	}
+	
 }
